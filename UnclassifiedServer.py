b589a6cdd688ff5f9a888ed40b6457298bce53aa
Harry <47015080+harryarthur@users.noreply.github.com>
2024-02-13 22:58:03 +1100

    Update UnclassifiedServer.py
    Removed unneeded code, added comments

--- UnclassifiedServer.py
+++ UnclassifiedServer.py
@@ -5,22 +5,20 @@ app = Flask(__name__)
 
 messages = []
 
+#define what happens when the POST method is called on localhost:5000/receive. Alice (or nay sender) will call this function
 @app.route('/receive', methods=['POST'])
 def receive_message():
     global messages
     message = request.json
     classification = message.get('Classification')
+    #forward message to protected server if classification is higher than Unclassified
     if classification != 'Unclassified':
         forward_message(message, 'http://localhost:5001')
     else:
         messages.append(message)
-#        distribute_message(message)
     return 'Message processed successfully at Unclassified Server', 200
 
-def forward_message(message, server_url):
-    response = requests.post(server_url + '/receive', json=message)
-    print(response.text)
-
+#define what happens when the GET method is called on localhost5000/get_messages. All receivers of unclassified messages will call this.
 @app.route('/get_messages', methods=['GET'])
 def get_messages():
     global messages
